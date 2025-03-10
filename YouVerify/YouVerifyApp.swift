9f2395cd62b6bde82d22772d51b192b63f5e0e5c
sampel65 <sampeldan65@gmail.com>
2025-03-10 13:46:28 +0100

    Initial Commit

---
+++ YouVerify/YouVerifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouVerifyApp.swift
+//  YouVerify
+//
+//  Created by Samson Oluwapelumi on 10/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouVerifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
